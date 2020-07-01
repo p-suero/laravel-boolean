e56b04dc3f952e9dc19fef6756c52a27944f0dbc
Paolo <paolo.suero@gmail.com>
2020-07-01 15:31:04 +0200

    Aggiunta animazione pagine delle faq

--- public/js/app.js
+++ public/js/app.js
@@ -37229,11 +37229,33 @@ module.exports = function(module) {
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var bootstrap__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! bootstrap */ "./node_modules/bootstrap/dist/js/bootstrap.js");
 /* harmony import */ var bootstrap__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(bootstrap__WEBPACK_IMPORTED_MODULE_0__);
-__webpack_require__(/*! ./bootstrap */ "./resources/js/bootstrap.js");
-
-var $ = __webpack_require__(/*! jquery */ "./node_modules/jquery/dist/jquery.js");
+__webpack_require__(/*! ./bootstrap */ "./resources/js/bootstrap.js"); //import j-query
 
 
+var $ = __webpack_require__(/*! jquery */ "./node_modules/jquery/dist/jquery.js"); //import bootstrap
+
+
+
+$(document).ready(function () {
+  //intercetto il click sulla domanda
+  $(".domanda").click(function () {
+    //salvo in variabile la domanda corrente
+    var domanda_corrente = $(this); //salvo in vatiabile la risposta corrente
+
+    var risposta_corrente = $(this).next(); //chiudo le eventuali risposte aperte
+
+    $(".risposta").slideUp(); //aggiungo l'icona di domanda non estesa
+
+    $(".domanda").find("i").removeClass("fa-minus").addClass("fa-plus"); //se la risposta corrente non é visibile
+
+    if (!risposta_corrente.is(":visible")) {
+      //mostro la risposta legata alla domanda corrente
+      risposta_corrente.slideDown(); //modifico l'icona che indica se la risposta è visibile o meno
+
+      domanda_corrente.find("i").toggleClass(" fa-plus fa-minus");
+    }
+  });
+});
 
 /***/ }),
 
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,26 @@
 require('./bootstrap');
-
+//import j-query
 var $ = require('jquery');
-
+//import bootstrap
 import 'bootstrap';
+
+$(document).ready(function() {
+    //intercetto il click sulla domanda
+    $(".domanda").click(function() {
+        //salvo in variabile la domanda corrente
+        var domanda_corrente = $(this);
+        //salvo in vatiabile la risposta corrente
+        var risposta_corrente = $(this).next();
+        //chiudo le eventuali risposte aperte
+        $(".risposta").slideUp();
+        //aggiungo l'icona di domanda non estesa
+        $(".domanda").find("i").removeClass("fa-minus").addClass("fa-plus");
+        //se la risposta corrente non é visibile
+        if (!risposta_corrente.is(":visible")) {
+            //mostro la risposta legata alla domanda corrente
+            risposta_corrente.slideDown();
+            //modifico l'icona che indica se la risposta è visibile o meno
+            domanda_corrente.find("i").toggleClass(" fa-plus fa-minus");
+        }
+    })
+})
